985757b5f36d2b3eb187fa9580e3aa1e1dd4af9e
Valeriy Sotnikov <sotnikov.link@mail.ru>
2017-06-07 15:32:42 +0300

    babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('Wow! It’s JS over webpack 2!');
+import path from 'path';
+
+alert('Wow! See console.log and src/index.js!');
+
+const o = {
+  a: 'ES7',
+  b: 'feature',
+  c: 'babel-polyfil',
+  d: path.resolve('C:/', 'Users', 'WoW', '..', 'Human')
+}
+
+console.log(
+  Object.values(o).join(', ') // ES7 feature Object.values
+);
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -3,9 +3,18 @@ var project = require('../project.config');
 
 module.exports = {
   context: path.resolve(project.ROOT, 'src'),
-  entry: { index: './index.js' },
+  entry: { index: ['babel-polyfill', './index.js'] },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(project.ROOT, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
   }
 };
