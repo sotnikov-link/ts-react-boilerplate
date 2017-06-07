4478c3f07603c690d6c12c03e884b922bf2c2763
Valeriy Sotnikov <sotnikov.link@mail.ru>
2017-06-07 18:15:21 +0300

    fix Object.assign  : )

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -2,13 +2,11 @@ var baseConfig = require('./base.config');
 var project = require('../project.config');
 var path = require('path');
 
-console.log(395, path.resolve(project.ROOT, 'static'));
-
-module.exports = Object.assign({
+module.exports = Object.assign({}, baseConfig, {
   devtool: 'eval',
   devServer: {
     open: true,
     contentBase: path.resolve(project.ROOT, 'static'),
     port: project.DEV_SERVER_PORT
   }
-}, baseConfig);
+});
